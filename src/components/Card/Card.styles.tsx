e35d3921555629acef01e25bbbd12fde9f34a7ca
Seolcita <seolcita@gmail.com>
2024-01-18 14:53:29 -0700

    Updated Card

--- src/components/Card/Card.styles.tsx
+++ src/components/Card/Card.styles.tsx
@@ -9,23 +9,27 @@ interface StyledCardProps {
   width?: CardProps['width'];
   height?: CardProps['height'];
   $margin?: string;
+  $hasBoxShadow: CardProps['hasBoxShadow'];
+  $borderRadius?: CardProps['borderRadius'];
 }
 
 const fixedBoxShadow = `0.3rem 0.35rem 0.5rem ${ColorMap['grey'].light}`;
 const fixedBackgroundColor = ColorMap['white'].main;
 
-export const StyledCard = styled.div<StyledCardProps>`
+export const StyledCard = styled.section<StyledCardProps>`
   position: relative;
   padding: ${({ $isPadded }) => ($isPadded ? space.lg : space.none)};
   background-color: ${fixedBackgroundColor};
-  box-shadow: ${fixedBoxShadow};
+  box-shadow: ${({ $hasBoxShadow }) =>
+    $hasBoxShadow ? fixedBoxShadow : 'none'};
   display: flex;
   justify-content: center;
   align-items: center;
-  border-radius: 0.5rem;
+  border-radius: ${({ $borderRadius }) => $borderRadius}rem;
   overflow: hidden;
   margin: ${({ $margin }) => $margin};
   width: ${({ width }) => (width ? `${width}rem` : '100%')};
+  cursor: ${({ $isInteractive }) => ($isInteractive ? 'pointer' : 'default')};
   ${({ height }) =>
     height &&
     css`
@@ -33,39 +37,20 @@ export const StyledCard = styled.div<StyledCardProps>`
     `};
 
   &:hover {
-    box-shadow: ${({ $isInteractive }) =>
-      $isInteractive
-        ? `0.4rem 0.5rem 0.65rem ${ColorMap['grey'].light}`
-        : fixedBoxShadow};
+    box-shadow: ${({ $isInteractive, $hasBoxShadow }) =>
+      $hasBoxShadow
+        ? $isInteractive
+          ? `0.4rem 0.5rem 0.65rem ${ColorMap['grey'].light}`
+          : fixedBoxShadow
+        : 'none'};
   }
 
   &:active {
-    box-shadow: ${({ $isInteractive }) =>
-      $isInteractive
-        ? `0.3rem 0.4rem 0.4rem ${ColorMap['grey'].light}`
-        : fixedBoxShadow};
-    background-color: ${({ $isInteractive }) =>
-      $isInteractive ? `${ColorMap['grey'].extraLight}` : fixedBackgroundColor};
-  }
-
-  &:before {
-    content: '';
-    position: absolute;
-    bottom: 0%;
-    left: 0%;
-    width: 100%;
-    height: 100%;
-    background-color: ${({ $isInteractive }) =>
-      $isInteractive
-        ? `${ColorMap['grey'].extraLight}80`
-        : fixedBackgroundColor};
-    z-index: -1;
-    border-radius: 0.5rem;
-  }
-
-  &:hover:before {
-    z-index: 3;
-    width: ${({ $isInteractive }) => ($isInteractive ? '100%' : '0%')};
-    height: ${({ $isInteractive }) => ($isInteractive ? '100%' : '0%')};
+    box-shadow: ${({ $isInteractive, $hasBoxShadow }) =>
+      $hasBoxShadow
+        ? $isInteractive
+          ? `0.3rem 0.4rem 0.4rem ${ColorMap['grey'].light}`
+          : fixedBoxShadow
+        : 'none'};
   }
 `;
